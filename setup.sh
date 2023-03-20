6df057c8e323a8250c8e9986c4d8cf5dcc2b7ab9
EliteScripts <78136872+EliteScripts@users.noreply.github.com>
2023-03-20 10:24:58 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp tourbillon.desktop /usr/share/applications/tourbillon.desktop
+
+cp tourbillon.py /usr/bin/tourbillon.py
+
+mkdir /usr/share/icons/hicolor/128x128/tourbillon/
+cp tourbillon.png /usr/share/icons/hicolor/128x128/tourbillon/tourbillon.png
