7b4e9929ec6fe4a21b3c9baa2c45e48981836dbb
EliteScripts <78136872+EliteScripts@users.noreply.github.com>
2023-03-30 18:32:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ cp tourbillon.desktop /usr/share/applications/tourbillon.desktop
 cp tourbillon.py /usr/bin/tourbillon.py
 
 mkdir /usr/share/icons/hicolor/128x128/tourbillon/
-cp tourbillon.png /usr/share/icons/hicolor/128x128/tourbillon/tourbillon.png
+cp tourbillon.png /usr/share/icons/hicolor/128x128/tourbillon.png
